3712cd5b45df3a27b4139fef060ceaa6bf9b76c2
daniel <Danielomosele@gmail.com>
2022-08-03 13:42:05 +0000

    add response code

--- src/africa/semicolon/callerapp/controllers/UserController.java
+++ src/africa/semicolon/callerapp/controllers/UserController.java
@@ -7,6 +7,9 @@ import africa.semicolon.callerapp.dtos.responses.AllContactResponse;
 import africa.semicolon.callerapp.dtos.responses.RegisterUserResponse;
 import africa.semicolon.callerapp.services.UserService;
 import africa.semicolon.callerapp.services.UserServiceImpl;
+import exception.UserExistsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,12 +19,21 @@ public class UserController {
 
     private UserService userService = new UserServiceImpl();
     @PostMapping("/user")
-    public RegisterUserResponse registerUSer(@RequestBody RegisterRequest request){
-        return  userService.registerUser(request);
+    public ResponseEntity<?> registerUSer(@RequestBody RegisterRequest request){
+        try {
+            RegisterUserResponse response = userService.registerUser(request);
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+        }
+        catch(UserExistsException ex){
+            return new ResponseEntity<>(ex.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+//    public RegisterUserResponse registerUSer(@RequestBody RegisterRequest request){
+//        return  userService.registerUser(request);
     }
     @PatchMapping("/user")
     public AddContactResponse addContact(@RequestBody AddContactRequest request){
         return userService.addContact(request);
+
     }
     @GetMapping("/user/{userEmail}")
     public List<AllContactResponse> findContactsBelongingTo(@PathVariable String userEmail){
